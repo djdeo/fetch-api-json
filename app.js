1a47dba9675b492d00839ea79f9b489c1f770f18
djdeo <deoseoer@gmail.com>
2017-12-20 16:37:16 +0800

    “add Date in Json

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ function getJson() {
     fetch('posts.json')
         .then(res => res.json())
         .then(data => {
-            let output = '';
+            let main = '', date = '', output = '';
             data.forEach(function(post) {
-                output += `
+                main += `
 				<div>
 					<h3>${post.title}</h3>
 					<p>${post.body}</p>
 				</div>
 				`;
             });
-            document.querySelector("#output").innerHTML = output;
+            console.log(data);
+            date = `<p><i><b>Last modified</b>: ${data[0].date}</i></p>`;
+            document.querySelector("#output").innerHTML = main + date;
         })
         .catch(err => console.log(err));
 }
